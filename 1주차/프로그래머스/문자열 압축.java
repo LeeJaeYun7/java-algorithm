558d70556cd8b5dc21b6b5d1b21978dc1aced0cd
LeeJaeYun <134198681+LeeJaeYun7@users.noreply.github.com>
2023-05-25 12:20:06 +0900

    Update 문자열 압축.java

--- 1주차/프로그래머스/문자열 압축.java
+++ 1주차/프로그래머스/문자열 압축.java
@@ -1,8 +1,18 @@
+// 왜 맞았는가?
+// 1. 문제의 조건에 유의해서, 식을 구현했다.
+// 2. ArrayList에 String을 저장하고, 그것이 이전꺼와 일치하는지 체크했다.
+// -> 일치하는 경우에는 cnt를 증가시키고, 일치하지 않는 경우에는 String ans에 추가해주었다.
+// 3. substring을 할 때, end가 최대끝을 넘지 않도록 해야 한다.
+// -> 그렇지 않으면, ArrayOutofBoundsException이 발생한다
+// 4. 다 끝나고 나서도, 남은 것에 대해서 한 번 ans에 더해줘야 한다.
+// -> 그래야, 남은 값들이 제대로 String ans에 더해진다. 
+
+
 import java.util.*; 
 
 class Solution {
     public int solution(String s) {
-         int len = s.length();
+      int len = s.length();
       int minLen = Integer.MAX_VALUE; 
       
       
