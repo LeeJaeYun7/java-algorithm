551d3168cfe6e096f512a27279ff6a22b8a6b070
LeeJaeYun <134198681+LeeJaeYun7@users.noreply.github.com>
2023-06-08 12:55:28 +0900

    Update 94. Binary Tree Inorder Traversal.java

--- 3주차/leetcode/94. Binary Tree Inorder Traversal.java
+++ 3주차/leetcode/94. Binary Tree Inorder Traversal.java
@@ -17,6 +17,11 @@ class Solution {
     
     public List<Integer> answer; 
     
+    // inorder 순회, 즉, 중위순회는
+    // 왼쪽, 현재 노드, 오른쪽 순으로 순회한다
+    // 따라서, 왼쪽 노드부터 재귀로 타면서 검색을 하고,
+    // 그 다음에 현재 노드의 값을 answer에 더해주고,
+    // 그 다음 오른쪽 노드를 재귀로 타면서 검색한다 
     public void recursive(TreeNode root){
         
         if(root == null){
